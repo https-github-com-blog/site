f1fcd0cfd0b5f8894315da5542526660d0ef506e
Ismail Aydin <i.aydin20014@gmail.com>
2020-02-26 11:52:20 +0100

    aydin

--- php/view/header.php
+++ php/view/header.php
@@ -8,15 +8,8 @@
 </head>
 <body>
     <header>
-
-    <h3>dfdsfdsf</h3>
-<<<<<<< HEAD
-    <h2>hi tout le monde</h2>
-    <h3>deneme son</h3>
-=======
-    <h1> lmgslknş </h1>
-    <h2>mereejfkjdkf</h2>
-    <h3>nsgnskgjmsdk</h3>
->>>>>>> 68c309212e22b638ae1660f363ab6bcdc862d109
+    
+    <h6>merhaba</h6>
+    <h3>dfsdfds</h3>
     </header>
     <main>
